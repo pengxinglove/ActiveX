a5552d7b3e32005d8bea75c53e87df1e5024c2cc
viswanath7dev <viswanath7dev@gmail.com>
2019-09-08 21:42:55 -0400

    files

---
+++ ActiveX_Web/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ActiveX_Web
+{
+    public class Class1
+    {
+        [ProgId("DemoCSharpActiveX.HelloWorld")]
+
+        [ClassInterface(ClassInterfaceType.AutoDual)]
+
+        [Guid("EDBFDE1E-5A09-4982-A638-91788B827716")]
+
+        [ComVisible(true)]
+
+        public class HelloWord
+
+        {
+
+            [ComVisible(true)]
+
+            public String SayHello()
+
+            {
+
+                return "Hello World!";
+
+            }
+
+        }
+
+    }
+}
+
